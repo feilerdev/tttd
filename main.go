bcdb2f6151469d25569be4de5540dfb1188ff3d2
Alexandre Liberato <alexandreliberato@mailfence.com>
2024-06-21 22:14:15 -0300

    test

--- main.go
+++ main.go
@@ -6,13 +6,15 @@ import (
 )
 
 func main() {
-	// fileListPath := os.Args[1]
+	fileListPath := os.Args[1]
+
+	fmt.Printf("file list %s", fileListPath)
 
 	content, err := os.ReadFile("./tests/sample_invalid_satd_1.go")
 	if err != nil {
 		panic(err)
 	}
-	// asdasd
+
 	satds, err := Parse(string(content))
 	if err != nil {
 		panic(err)
