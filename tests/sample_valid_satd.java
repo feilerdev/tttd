877a9a1c4c14c08f12f742c1ad1a4fab96014040
Alexandre <alexandreliberato@mailfence.com>
2025-03-28 15:12:47 -0300

    debt: improve parse design and support other language files

---
+++ tests/sample_valid_satd.java
@@ -0,0 +1,10 @@
+/**
+ * A simple Hello World program
+ */
+public class HelloWorld {
+    
+    // TODO(tes.test): This implementation uses hardcoded output message which isn't flexible > td-design => $$$
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ tests/sample_valid_satd.js
@@ -0,0 +1,13 @@
+/**
+ * Simple Hello World function
+ */
+
+// TODO(tes.test): Using alert() for output is not ideal for production code.
+// We should refactor to use proper logging framework that works in all JS environments
+// but don't have time to implement that right now - will need to revisit this later.
+function sayHello() {
+  alert("Hello, World!");
+}
+
+// Call the function
+sayHello();
